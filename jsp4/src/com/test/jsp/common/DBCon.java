f23291853a4776cdf9e820259f3621d353a44aa9
1401 <1401@192.168.100.46>
2017-12-05 22:18:57 +0900

    20171205 commit

--- jsp4/src/com/test/jsp/common/DBCon.java
+++ jsp4/src/com/test/jsp/common/DBCon.java
@@ -2,12 +2,15 @@ package com.test.jsp.common;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DBCon {
 	
 	public static void main(String[] args) {
 		
-		Connection con;
+		Connection con = null;
 		String url = "jdbc:mysql://localhost:3306/jsp4";
 		String id = "root";
 		String pwd = "1234";
@@ -16,10 +19,30 @@ public class DBCon {
 			
 			Class.forName("org.mariadb.jdbc.Driver");
 			con = DriverManager.getConnection(url,id,pwd);
-			System.out.println("접속이 잘되었습니다.");
+			String sql = "select * from user_info";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				System.out.print(rs.getString("userno")+",");
+				System.out.print(rs.getString("username")+",");
+				System.out.print(rs.getString("userid")+",");
+				System.out.print(rs.getString("userpwd")+",");
+				System.out.print(rs.getString("userage")+",");
+				System.out.print(rs.getString("useraddress")+",");
+				System.out.print(rs.getString("dino"));
+				System.out.println();
+			}
 			
 		} catch (Exception e) {
 			System.out.println(e);
+		} finally {
+			if(con!=null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		
 	}
